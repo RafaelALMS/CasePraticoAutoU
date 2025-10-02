51f156e48b2f79c5ccde15d80af6d1dd18e8bcec
RafaelALMS <eafaelsilva@gmail.com>
2025-10-02 18:54:26 -0300

    Fixed:Problema com autorização com CORS

--- main.py
+++ main.py
@@ -54,20 +54,14 @@ async def usefulEmail(request: EmailRequest):
 
 
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # This is important for Vercel
 if __name__ == "__main__":
